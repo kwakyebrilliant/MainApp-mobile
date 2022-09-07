c2af8300bd078b2a8dc85d4c8bf0dc53df300290
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-07 23:47:30 +0000

    app updates

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View, Linking} from 'react-native';
 
   const App = () => {
   return (
     <View style={styles.body}>
       <Text style={styles.text}>Programming with Brilliant</Text>
-      <Button title="learn coding"></Button>
+      <Button title="learn coding" onPress={() => {Linking.openURL('https://www.youtube.com/watch?v=1lg_IXtjles&list=PL8kfZyp--gEXs4YsSLtB3KqDtdOFHMjWZ&index=6')}}></Button>
     </View>
   );
 };
