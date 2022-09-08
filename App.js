148e61117159e12005532eea65b95dca9f8f9bed
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 14:12:50 +0000

    app updates

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, TextInput, Pressable} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Alert,
+  ToastAndroid,
+  Modal,
+} from 'react-native';
 
 const App = () => {
   const [name, SetName] = useState('');
@@ -42,6 +51,7 @@ const styles = StyleSheet.create({
     color: '#000000',
     fontSize: 20,
     margin: 10,
+    textAlign: 'center',
   },
   input: {
     width: 200,
@@ -56,6 +66,38 @@ const styles = StyleSheet.create({
     width: 150,
     height: 50,
     alignItems: 'center',
+  },
+  centered_view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000099',
+  },
+  warning_modal: {
+    width: 300,
+    height: 300,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 20,
+  },
+  warning_title: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff0',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  warning_button: {
+    backgroundColor: '#00ffff',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
 });
 
