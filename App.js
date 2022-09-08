80155e0937c6083a3300c3b49db707879f67d434
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 00:25:57 +0000

    app updates

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import {Button, StyleSheet, Text, View, Linking} from 'react-native';
 
   const App = () => {
     const [name, setName] = useState('Brilliant');
+    const [session, setSession] = useState({number: 6, title: 'state'});
     const onClickHandler = () => {
       setName('Programming with Brilliant')
     }
   return (
     <View style={styles.body}>
       <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>This session number is {session.number} and about {session.title}</Text>
       <Button title="Update state" onPress={onClickHandler}></Button>
     </View>
   );
