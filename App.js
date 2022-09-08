144664f4e052be2c4306aeeb2069f27a405cbfb9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 00:28:23 +0000

    error fixed

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View, Linking} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-  const App = () => {
-    const [name, setName] = useState('Brilliant');
-    const [session, setSession] = useState({number: 6, title: 'state'});
-    const onClickHandler = () => {
-      setName('Programming with Brilliant')
-    }
+const App = () => {
+  const [name, setName] = useState('Brilliant');
+  const [session, setSession] = useState({number: 6, title: 'state'});
+  const onClickHandler = () => {
+    setName('Programming with Brilliant');
+    setSession({number: 7, title: 'Style'});
+  };
   return (
     <View style={styles.body}>
       <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>This session number is {session.number} and about {session.title}</Text>
-      <Button title="Update state" onPress={onClickHandler}></Button>
+      <Text style={styles.text}>
+        This session number is {session.number} and about {session.title}
+      </Text>
+      <Button title="Update state" onPress={onClickHandler} />
     </View>
   );
 };
