b0dc80899f88971b2403042a33c8f2cff0e60d4d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 09:51:55 +0000

    app updates

--- App.js
+++ App.js
@@ -4,15 +4,6 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View style={styles.body}>
-    <View style={styles.view1}>
-      <Text style={styles.text}>1</Text>
-    </View>
-    <View style={styles.view2}>
-      <Text style={styles.text}>2</Text>
-    </View>
-    <View style={styles.view3}>
-      <Text style={styles.text}>3</Text>
-    </View>
   </View>
   );
 };
@@ -21,37 +12,10 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 10,
-    borderColor: '#ff00ff',
     borderRadius: 10,
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 1,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 35,
-    fontStyle: 'italic',
-    margin: 10,
-    textTransform: 'uppercase',
   },
 });
 
