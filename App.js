856414001fafe53d98f454b855124518d3ed76fd
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 09:58:18 +0000

    app updates

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {key: 1, item: 'item 1'},
+    {key: 2, item: 'item 2'},
+    {key: 3, item: 'item 3'},
+    {key: 4, item: 'item 4'},
+    {key: 5, item: 'item 5'},
+    {key: 6, item: 'item 6'},
+    {key: 7, item: 'item 7'},
+    {key: 8, item: 'item 8'},
+    {key: 44, item: 'item 9'},
+    {key: 68, item: 'item 27'},
+    {key: 0, item: 'item 78'},
+  ]);
   return (
     <View style={styles.body}>
+      <Text style={styles.text}>Item 1</Text>
   </View>
   );
 };
@@ -16,6 +30,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
+  },
+  text: {
+    color: '#000000',
+    fontSize: 45,
+    fontStyle: 'italic',
+    margin: 10,
   },
 });
 
