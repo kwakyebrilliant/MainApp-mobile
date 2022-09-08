24a31349b017b5eb37a67f8b4b0cac30a46cbfd3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 10:23:40 +0000

    app updates

--- App.js
+++ App.js
@@ -1,47 +1,36 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, ScrollView, RefreshControl} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, SectionList} from 'react-native';
 
 const App = () => {
-  const [Items, setItems] = useState([
-    {key: 1, item: 'item 1'},
-    {key: 2, item: 'item 2'},
-    {key: 3, item: 'item 3'},
-    {key: 4, item: 'item 4'},
-    {key: 5, item: 'item 5'},
-    {key: 6, item: 'item 6'},
-    {key: 7, item: 'item 7'},
-    {key: 8, item: 'item 8'},
-    {key: 44, item: 'item 9'},
-    {key: 68, item: 'item 27'},
-    {key: 0, item: 'item 78'},
-  ]);
-  const [Refreshing, setRefreshing] = useState(false);
-  const onRefresh = () => {
-    setRefreshing(true);
-    setItems([...Items, {key: 69, item: 'Item 69'}]);
-    setRefreshing(false);
-  }
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1'],
+    },
+    {
+      title: 'Title 4',
+      data: ['Item 4-1', 'Item 4-2'],
+    },
+  ];
   return (
-    <ScrollView
-      style={styles.body}
-      refreshControl={
-        <RefreshControl
-          refreshing={Refreshing}
-          onRefresh={onRefresh}
-          colors={['#ff00ff']}
-        />
-      }
-    >
-      {
-        Items.map((object) => {
-          return (
-            <View style={styles.item} key={object.key}>
-              <Text style={styles.text}>{object.item}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView>
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={DATA}
+      renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+      renderSectionHeader={({section}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{section.title}</Text>
+        </View>
+      )}
+    />
   );
 };
 
