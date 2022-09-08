08a81f935021d5b187e6c4be8a725642a9ba41b0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 00:37:36 +0000

    updates

--- App.js
+++ App.js
@@ -2,23 +2,13 @@ import React, {useState} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Brilliant');
-  const [session, setSession] = useState({number: 6, title: 'state'});
-  const [current, setCurrent] = useState(true);
+  const [name, setName] = useState('Style test');
   const onClickHandler = () => {
     setName('Programming with Brilliant');
-    setSession({number: 7, title: 'Style'});
-    setCurrent(false);
   };
   return (
     <View style={styles.body}>
       <Text style={styles.text}>{name}</Text>
-      <Text style={styles.text}>
-        This session number is {session.number} and about {session.title}
-      </Text>
-      <Text style={styles.text}>
-        {current ? 'current session' : 'next session'}
-      </Text>
       <Button title="Update state" onPress={onClickHandler} />
     </View>
   );
