5b1af912229378aaec9589c5d0f9da32914bfbe3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-08 00:08:26 +0000

    usetate added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {Button, StyleSheet, Text, View, Linking} from 'react-native';
     const [name, setName] = useState('Brilliant');
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Programming with Brilliant</Text>
+      <Text style={styles.text}>My name is {name}</Text>
       <Button title="learn coding" onPress={() => {Linking.openURL('https://www.youtube.com/watch?v=1lg_IXtjles&list=PL8kfZyp--gEXs4YsSLtB3KqDtdOFHMjWZ&index=6')}}></Button>
     </View>
   );
