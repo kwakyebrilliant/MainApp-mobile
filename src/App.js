cc46f52de141ea0f9a0e141b22fdf142e23d917a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 19:11:13 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
- 
+
 function ScreenA() {
   return (
     <View>
