32cb3c9d9cda66b0ff118f878a9b3959db4343ef
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 22:08:31 +0000

    profile updates

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
+import styles from './styles';
 
 const Profile = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Profile</Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Profile;
