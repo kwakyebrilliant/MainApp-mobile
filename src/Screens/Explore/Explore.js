271c3c13d5f14b2c3ff9d00f0adc1cb7a14ba752
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 23:05:38 +0000

    explore updates

---
+++ src/Screens/Explore/Explore.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Explore = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Explore</Text>
+    </View>
+  );
+};
+
+export default Explore;
