8277b13bf1d5899826322f47ab7bbc85bfdea0a1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 23:10:01 +0000

    explore updates

--- src/Navigations/Routes.js
+++ src/Navigations/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, Profile} from '../Screens';
+import {Home, Profile, Explore} from '../Screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +11,7 @@ function Routes() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Explore" component={Explore} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/Screens/Explore/Explore.js
+++ src/Screens/Explore/Explore.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import styles from './styles';
+import styles from '../Home/styles';
 
 const Explore = () => {
   return (
