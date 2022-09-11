b7348c6531e9a3723c1588a19dd129c083ede8a6
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 20:28:39 +0000

    updates

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+function App() {
+  return (
+   <View>
+    <Text>Here</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
