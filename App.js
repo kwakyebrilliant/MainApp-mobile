55b0beba0fe5388b2af8b2b945b7f3e2e1eaf223
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-11 21:15:40 +0000

    app updates

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
 const App = () => {
   return (
-   <View style={styles.container}>
-    <Text>Here</Text>
-   </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
