52c2871b2032402270ca26e57fce53bf007055f1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 16:43:48 +0000

    updates

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 
-const Profile = (props) => {
-  const {navigation, route} = props;
-  console.log('routes', route);
+const Profile = ({navigation, route}) => {
+  const {title} = route.params;
+  console.log('routes', title);
   return (
     <View style={styles.container}>
+      <Text>{title}</Text>
       <Text>Profile</Text>
     </View>
   );
