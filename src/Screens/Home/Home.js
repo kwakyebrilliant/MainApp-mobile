28124da793b7a7384be52b72057361becb5b380d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 17:29:15 +0000

    home updates

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -5,7 +5,7 @@ import styles from './styles';
 
 const Home = ({navigation}) => {
   const goToScreen = () => {
-    navigation.replace(navigationStrings.PROFILE, {title: 'Subscribe'});
+    navigation.navigate(navigationStrings.PROFILE, {title: 'Subscribe'});
   };
   return (
     <View style={styles.container}>
