d87cdd2d0e73029fc003b042c7bfe9b50ec3a1da
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 12:10:13 +0000

    home updates

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 import styles from './styles';
 
-const Home = (props) => {
-  console.log('props available', props);
+const Home = ({navigation}) => {
+  console.log('props available', navigation);
   return (
     <View style={styles.container}>
-      <Button onPress={() => alert('Pressed')} title="Go To Profile" />
+      <Button
+        onPress={() => navigation.navigate('Profile')}
+        title="Go To Profile"
+      />
     </View>
   );
 };
