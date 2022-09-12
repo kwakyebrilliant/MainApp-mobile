840ba94516ae647206ea816b8c58b2402770b542
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 21:00:02 +0000

    route updates

--- src/Navigations/Routes.js
+++ src/Navigations/Routes.js
@@ -11,7 +11,8 @@ function Routes() {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName={navigationStrings.HOME}
-        screenOptions={{headerShown: false}}>
+        // screenOptions={{headerShown: false}}
+      >
         <Stack.Screen
           options={{title: 'My Home'}}
           name={navigationStrings.HOME}
