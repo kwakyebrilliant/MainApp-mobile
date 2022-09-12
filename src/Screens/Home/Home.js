991018359f503e536b4c7400fc98dba0ffac90e7
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 11:58:28 +0000

    home updates

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 import styles from './styles';
 
-const Home = () => {
+const Home = (props) => {
+  console.log('props available', props);
   return (
     <View style={styles.container}>
       <Button onPress={() => alert('Pressed')} title="Go To Profile" />
