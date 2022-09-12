b31fbd04b0f457e5a88e69440f8a9068f612ce00
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 20:26:09 +0000

    header updates

---
+++ src/Components/HeaderComp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HeaderComp = ({navigation, route}) => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+};
+
+export default HeaderComp;
