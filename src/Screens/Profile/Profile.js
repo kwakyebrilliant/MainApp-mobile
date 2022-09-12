b8728702e34c9612c38931a31be95d281f94bb62
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-12 17:24:03 +0000

    profile updates

--- src/Screens/Profile/Profile.js
+++ src/Screens/Profile/Profile.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import navigationStrings from '../../constants/navigationStrings';
 import styles from './styles';
 
 const Profile = ({navigation, route}) => {
   const {title} = route.params;
   console.log('routes', title);
+  const goToScreen = () => {
+    navigation.navigate(navigationStrings.PROFILE, {title: 'Subscribe'});
+  };
   return (
     <View style={styles.container}>
+      <Text>This is Profile Screen</Text>
       <Text>{title}</Text>
       <Text>Profile</Text>
     </View>
