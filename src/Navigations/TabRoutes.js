918c7755734e87b531986ce2ec737a65a8814d54
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-13 14:34:34 +0000

    tab updates

---
+++ src/Navigations/TabRoutes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Home, Profile, Explore} from '../Screens';
+import navigationStrings from '../constants/navigationStrings';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function TabRoutes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={navigationStrings.HOME}
+        screenOptions={{headerShown: false}}>
+        <Tab.Screen
+          options={{title: 'My Home'}}
+          name={navigationStrings.HOME}
+          component={Home}
+        />
+        <Tab.Screen name={navigationStrings.PROFILE} component={Profile} />
+        <Tab.Screen name={navigationStrings.EXPLORE} component={Explore} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default TabRoutes;
