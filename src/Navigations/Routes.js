d8940bd0a9ae9f2b65f32deb5556471b515b0bc3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-13 13:46:37 +0000

    routes updates

--- src/Navigations/Routes.js
+++ src/Navigations/Routes.js
@@ -3,8 +3,10 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Home, Profile, Explore} from '../Screens';
 import navigationStrings from '../constants/navigationStrings';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function Routes() {
   return (
