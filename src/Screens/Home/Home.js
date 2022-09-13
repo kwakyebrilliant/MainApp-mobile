0fa84f025dd53993b0995d32fbdd2001ee6f6993
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-13 14:00:55 +0000

    home updates

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import {View, Text, Button, SafeAreaView} from 'react-native';
-import HeaderComp from '../../Components/HeaderComp';
-import navigationStrings from '../../constants/navigationStrings';
-import styles from './styles';
+import {View, Text} from 'react-native';
+import styles from '../Home/styles';
 
-const Home = ({navigation}) => {
-  const goToScreen = () => {
-    navigation.navigate(navigationStrings.PROFILE, {title: 'Subscribe'});
-  };
+const Home = () => {
   return (
     <View style={styles.container}>
-      <SafeAreaView>
-        <HeaderComp goBack={() => navigation.goBack} />
-        <Text>This is Home Screen</Text>
-        <Button onPress={goToScreen} title="Go To Profile" />
-      </SafeAreaView>
+      <Text>Home</Text>
     </View>
   );
 };
