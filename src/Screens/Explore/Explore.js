491a36ee7ae0fdab776406ad36267a0d0634d111
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-13 14:02:19 +0000

    explore updates

--- src/Screens/Explore/Explore.js
+++ src/Screens/Explore/Explore.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import navigationStrings from '../../constants/navigationStrings';
+import {View, Text} from 'react-native';
 import styles from '../Home/styles';
 
-const Explore = ({navigation}) => {
-  const goToScreen = () => {
-    navigation.popToTop();
-  };
+const Explore = () => {
   return (
     <View style={styles.container}>
       <Text>Explore</Text>
-      <Button onPress={goToScreen} title="Go Back" />
     </View>
   );
 };
