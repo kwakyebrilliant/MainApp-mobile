7a9b08e2725aaf82f4be09df898e89ba0ebb7d9c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-14 21:12:36 +0000

    routes upadtaes

--- src/Navigations/Routes.js
+++ src/Navigations/Routes.js
@@ -11,17 +11,7 @@ const Tab = createBottomTabNavigator();
 function Routes() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={navigationStrings.HOME}
-        screenOptions={{headerShown: false}}>
-        <Tab.Screen
-          options={{title: 'My Home'}}
-          name={navigationStrings.HOME}
-          component={Home}
-        />
-        <Tab.Screen name={navigationStrings.PROFILE} component={Profile} />
-        <Tab.Screen name={navigationStrings.EXPLORE} component={Explore} />
-      </Tab.Navigator>
+      <Stack.Navigator />
     </NavigationContainer>
   );
 }
