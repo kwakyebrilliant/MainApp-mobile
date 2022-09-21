e4463971ae0bc623718b75a33c3a1b09a92a7c2f
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-09-21 10:20:01 +0000

    home updates

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import styles from '../Home/styles';
 
 const Home = () => {
+  const images = [
+    require('../../../assets/done.png'),
+    require('../../../assets/done.png'),
+    require('../../../assets/done.png'),
+    require('../../../assets/done.png'),
+    require('../../../assets/done.png'),
+  ];
   return (
     <View style={styles.container}>
       <Text>Home alone</Text>
